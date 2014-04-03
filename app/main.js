3db6288e7b2d6bd73608f42f95f0e2223a4323a8
Calvin Spealman <calvin@caktusgroup.com>
2014-04-03 16:31:57 -0400

    Implemented the first shifting: up one cell if the above cell is free.

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+/* global pillstreak */
 window.pillstreak = (function() {
     var exports = {
         init: function() {
@@ -5,7 +6,7 @@ window.pillstreak = (function() {
             var row, col;
             for (var i=0; i<this.cells.length; i++) {
                 row = i % 4;
-                col = parseInt(i / 4);
+                col = parseInt(i / 4, 10);
                 this.cells[i].setAttribute('type', 'free');
                 this.cells[i].setAttribute('row', row);
                 this.cells[i].setAttribute('col', col);
@@ -19,13 +20,79 @@ window.pillstreak = (function() {
                 }
             }
             if (free.length > 0) {
-                return free[ parseInt(free.length * Math.random()) ];
+                return free[ parseInt(free.length * Math.random(), 10) ];
             }
         },
         setCell: function(cell, data) {
             var prop;
             for (prop in data) {
                 cell.setAttribute(prop, data[prop]);
+            }
+        },
+
+        getNeighbor: function(cell, direction) {
+            var row = parseInt(cell.getAttribute('row'), 10); 
+            var col = parseInt(cell.getAttribute('col'), 10); 
+
+            switch (direction) {
+                case 'up':
+                    row--;
+                    break;
+                case 'down':
+                    row++;
+                    break;
+                case 'left':
+                    col--;
+                    break;
+                case 'right':
+                    col++;
+                    break;
+                default:
+                    break;
+            }
+
+            if (row < 0 || row > 3 || col < 0 || col > 3) {
+                return null;
+            } else {
+                return this.getCellAt(row, col);
+            }
+        },
+
+        getCellAt: function(row, col) {
+            return document.querySelector(".cell[col='"+col+"'][row='"+row+"']");
+        },
+
+        getRow: function(row) {
+            var cells = document.querySelectorAll(".cell[row='"+row+"']");
+            cells.forEach = Array.prototype.forEach;
+            return cells;
+        },
+
+        swapCells: function(cell, other) {
+            var first_row = cell.getAttribute('row');
+            var first_col = cell.getAttribute('col');
+
+            cell.setAttribute('row', other.getAttribute('row'));
+            cell.setAttribute('col', other.getAttribute('col'));
+
+            other.setAttribute('row', first_row);
+            other.setAttribute('col', first_col);
+        },
+
+        shiftUp: function() {
+            var neighbor;
+            
+            function shiftRowUp(cell) {
+                if (cell.getAttribute('type') !== 'free') {
+                    neighbor = pillstreak.getNeighbor(cell, 'up');
+                    if (neighbor && neighbor.getAttribute('type') === 'free') {
+                        pillstreak.swapCells(cell, neighbor);
+                    }
+                }
+            }
+
+            for (var i = 0; i < 4; i ++) {
+                this.getRow(i).forEach(shiftRowUp);
             }
         },
     };
