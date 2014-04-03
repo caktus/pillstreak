a5645f08a6450ff1174f25874ea0947fc5be468a
Calvin Spealman <calvin@caktusgroup.com>
2014-04-03 16:11:21 -0400

    Simple facility for setting cell types

--- app/main.js
+++ app/main.js
@@ -1,15 +1,35 @@
-require.config({ 
-    paths: {
-        a: '../modules/a',
-        b: '../modules/b',
-        c: '../modules/c',
-        jquery: '../lib/jquery'
-    }
-});
+window.pillstreak = (function() {
+    var exports = {
+        init: function() {
+            this.cells = document.querySelectorAll(".cell");
+            var row, col;
+            for (var i=0; i<this.cells.length; i++) {
+                row = i % 4;
+                col = parseInt(i / 4);
+                this.cells[i].setAttribute('type', 'free');
+                this.cells[i].setAttribute('row', row);
+                this.cells[i].setAttribute('col', col);
+            }
+        },
+        getFreeCell: function() {
+            var free = [];
+            for (var i=0; i<this.cells.length; i++) {
+                if (this.cells[i].getAttribute('type') === 'free') {
+                    free.push(this.cells[i]);
+                }
+            }
+            if (free.length > 0) {
+                return free[ parseInt(free.length * Math.random()) ];
+            }
+        },
+        setCell: function(cell, data) {
+            var prop;
+            for (prop in data) {
+                cell.setAttribute(prop, data[prop]);
+            }
+        },
+    };
+    return exports;
+})();
 
-require(['a', 'b', 'c', 'jquery'], function (a, b, c, $) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log($);
-});
+window.pillstreak.init();
