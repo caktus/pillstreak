ac05bd1ea69e9fd15154f8948682e1ed457e023f
Calvin Spealman <calvin@caktusgroup.com>
2014-04-04 14:46:21 -0400

    Disable all browser touch, so we don't scroll and toggle the address bar while trying to play.

--- app/main.js
+++ app/main.js
@@ -69,6 +69,10 @@ window.pillstreak = (function() {
                 pillstreak.shiftAllCells(ev.gesture.direction);
             });
 
+            document.body.ontouchstart = function(ev) {
+                ev.preventDefault();
+            };
+
             setInterval(function() {
                 pillstreak.tick();
             }, CONF.TICK);
